e7016125af6cb3c39f124d50588ce0a7548258ac
Chaitanya Priyadarshi <chaitanya.priyadarshi@ifj.edu.pl>
2026-07-07 14:35:07 +0200

    Update talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,9 +5,14 @@ var addressPoints = [
     -0.3763353
   ],
   [
-    "Analysis with First Data from Large Size Telescope of Cherenkov Telescope Array<br />Centro de Ciencias de Benasque Pedro Pascual; Benasque, Spain",
-    42.6052663,
-    0.5237318
+    "Can we see light in the ultra-high-energy sky?<br />Saha Institute of Nuclear Physics; Kolkata, India",
+    22.5726459,
+    88.3638953
+  ],
+  [
+    "Search for UHE photon sources using direction-time clustering algorithms with a BDT particle discriminant<br />Hospederia San Martin Pinario; Santiago de Compostela, Spain",
+    42.9064576,
+    -8.5192983
   ],
   [
     "Looking at the night sky in the very high energy range<br />remote; Bhopal, India",
@@ -15,14 +20,14 @@ var addressPoints = [
     77.401989
   ],
   [
-    "Can we see light in the ultra-high-energy sky?<br />Saha Institute of Nuclear Physics; Kolkata, India",
-    22.5726459,
-    88.3638953
-  ],
-  [
     "Space-Time Clustering analysis using Python<br />Centro Convegni 'Luigi Zordan'; L'Aquila, Italy",
     42.1368853,
     13.610341
+  ],
+  [
+    "Analysis with First Data from Large Size Telescope of Cherenkov Telescope Array<br />Centro de Ciencias de Benasque Pedro Pascual; Benasque, Spain",
+    42.6052663,
+    0.5237318
   ],
   [
     "Ultra-High-Energy Photon Searches with the Pierre Auger Observatory: Constraints, Multi-messenger Synergies, and Future Prospects<br />The University-Enterprise Foundation of the University of Valencia (ADEIT); Valencia, Spain",
